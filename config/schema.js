88efca3373075bd28f947706099c4c6a7737b87f
sunjilkh <110080955+sunjilkh@users.noreply.github.com>
2025-05-20 13:24:27 +0600

    schema

--- config/schema.js
+++ config/schema.js
@@ -5,3 +5,18 @@ export const usersTable = pgTable("users", {
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
+
+export const coursesTable = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  cid: varchar().notNull(),
+  name: varchar().notNull(),
+  description: varchar().notNull(),
+  no0fChapters: integer().notNull(),
+  includeVideo: boolean().default(false),
+  level: varchar().notNull(),
+  catetgory: varchar().notNull(),
+  courseJson: json(),
+  userEmail: varchar("userEmail")
+    .references(() => usersTable.email)
+    .notNull(),
+});
