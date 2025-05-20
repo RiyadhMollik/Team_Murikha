31782f597f286f2c17af94d4fc853e0db1ec9b29
sunjilkh <110080955+sunjilkh@users.noreply.github.com>
2025-05-20 20:36:22 +0600

    error

---
+++ app/api/generate-course-layout/route.jsx
@@ -0,0 +1,80 @@
+import { NextResponse } from "next/server";
+import { GoogleGenAI } from "@google/genai";
+// import { currentUser } from "@clerk/nextjs/dist/types/server";
+import { currentUser } from "@clerk/nextjs/server";
+
+
+const PROMPT = `Genrate Learning Course depends on following details. In which Make sure to add Course Name, Description,Course Banner Image Prompt (Create a modern, flat-style 2D digital illustration representing user Topic. Include UI/UX elements such as mockup screens, text blocks, icons, buttons, and creative workspace tools. Add symbolic elements related to user Course, like sticky notes, design components, and visual aids. Use a vibrant color palette (blues, purples, oranges) with a clean, professional look. The illustration should feel creative, tech-savvy, and educational, ideal for visualizing concepts in user Course) for Course Banner in 3d format Chapter Name, , Topic under each chapters , Duration for each chapters etc, in JSON format only
+
+Schema:
+
+{
+  "course": {
+    "name": "string",
+    "description": "string",
+    "category": "string",
+    "level": "string",
+    "includeVideo": "boolean",
+    "noOfChapters": "number",
+
+"bannerImagePrompt": "string",
+    "chapters": [
+      {
+        "chapterName": "string",
+        "duration": "string",
+        "topics": [
+          "string"
+        ],
+     
+      }
+    ]
+  }
+}
+
+, User Input: 
+
+`;
+
+export async function POST(req) {
+  const formData = await req.json();
+  const user = await currentUser();
+  const ai = new GoogleGenAI({
+    // apiKey: process.env.GEMINI_API_KEY,
+    apiKey: "AIzaSyBWzGwEWitgNGPCDoyrmThes00cgESyeus",
+  });
+  const config = {
+    responseMimeType: "text/plain",
+  };
+//   const model = "gemma-3-12b-it";
+const model = 'gemini-2.0-flash';
+  const contents = [
+    {
+      role: "user",
+      parts: [
+        {
+          text: PROMPT + JSON.stringify(formData),
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContent({
+    model,
+    config,
+    contents,
+  });
+  console.log(response?.cordinates[0]?.content.parts[0]?.text);
+
+const RawRes = response?.cordinates[0]?.content.parts[0]?.text;
+const RawJson = RawRes?.split("```json")[1].split("```")[0];
+const JSONRes = JSON.parse(RawJson);
+  //Save the response to the database
+//   const course = await db.insert(coursesTable).values({
+//     ...formData,
+//     courseJson: response.text(),
+//     userEmail: user?.primaryEmailAddress?.emailAddress,
+//   });
+
+  
+  return NextResponse.json(JSONRes);
+}
--- app/workspace/_components/AddNewCourseDialog.jsx
+++ app/workspace/_components/AddNewCourseDialog.jsx
@@ -1,79 +1,155 @@
-import React from 'react'
+import React, { useState } from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "../../../@/components/ui/dialog"
-import { Input } from '../../../@/components/ui/input'
-import { Textarea } from '../../../@/components/ui/Textarea'
-import { Switch } from '../../../@/components/ui/switch'
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../../@/components/ui/dialog";
+import { Input } from "../../../@/components/ui/input";
+import { Textarea } from "../../../@/components/ui/Textarea";
+import { Switch } from "../../../@/components/ui/switch";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "../../../@/components/ui/select"
-import { Button } from '../../../@/components/ui/button'
-import { Sparkle } from 'lucide-react'
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../../@/components/ui/select";
+import { Button } from "../../../@/components/ui/button";
+import { Loader2Icon, Sparkle } from "lucide-react";
+import { desc } from "drizzle-orm";
+import axios from "axios";
 
 function AddNewCourseDialog({ children }) {
-    return (
-        <Dialog>
-            <DialogTrigger asChild>{children}</DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle className={'text-2xl font-bold text-center'}>Create New Course Using AI</DialogTitle>
-                    <DialogDescription asChild>
-                        <div className='flex flex-col gap-4 mt-3'>
-                            <div className='flex flex-col gap-2'>
-                                <label>Course Name</label>
-                                <Input placeholder="Enter Course Name" />
-                            </div>
-                            <div className='flex flex-col gap-2'>
-                                <label>Course Description</label>
-                                <Textarea placeholder="Enter Course Description" />
-                            </div>
-                            <div className='flex flex-col gap-2'>
-                                <label>number of chapters</label>
-                                <Input placeholder="Enter number of chapters" />
-                            </div>
-                            <div className='flex items-center gap-3'>
-                                <label>include video</label>
-                                <Switch />
-                            </div>
-                            <div className='flex flex-col gap-2'>
-                                <label>Deficulty Level</label>
-                                <Select>
-                                    <SelectTrigger className="w-full">
-                                        <SelectValue placeholder="Deficulty Level" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value="beginner">Beginner</SelectItem>
-                                        <SelectItem value="intermediate">Intermediate</SelectItem>
-                                        <SelectItem value="advanced">Advance</SelectItem>
-                                    </SelectContent>
-                                </Select>
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    noOfChapters: 1,
+    includeVideo: false,
+    level: "",
+    category: "",
+  });
 
-                            </div>
-                            <div className='flex flex-col gap-2'>
-                                <label>Category</label>
-                                <Input placeholder="Enter Category(sperated by Comma)" />
-                            </div>
+  const handleInputChange = (field, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+    console.log(formData);
+  };
 
-                            <div>
-                                <Button className='flex items-center w-full'> <Sparkle className='mr-2' /> Generate Course</Button>
-                            </div>
-                        </div>
-                    </DialogDescription>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
+  const onGenerateCourse = () => {
+    console.log(formData);
+  };
 
-    )
+  const onGenerate = async () => {
+    console.log(formData);
+    try {
+      setIsLoading(true);
+      const result = await axios.post("/api/generate-course-layout", {
+        ...formData,
+      });
+      console.log(result.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className={"text-2xl font-bold text-center"}>
+            Create New Course Using AI
+          </DialogTitle>
+          <DialogDescription asChild>
+            <div className="flex flex-col gap-4 mt-3">
+              <div className="flex flex-col gap-2">
+                <label>Course Name</label>
+                <Input
+                  placeholder="Enter Course Name"
+                  onChange={(event) =>
+                    handleInputChange("courseName", event?.target.value)
+                  }
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label>Course Description</label>
+                <Textarea
+                  placeholder="Enter Course Description"
+                  onChange={(event) =>
+                    handleInputChange("description", event?.target.value)
+                  }
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label>number of chapters</label>
+                <Input
+                  placeholder="Enter number of chapters"
+                  onChange={(event) =>
+                    handleInputChange("noOfChapters", event?.target.value)
+                  }
+                />
+              </div>
+              <div className="flex items-center gap-3">
+                <label>include video</label>
+                <Switch
+                  checked={formData.includeVideo}
+                  onCheckedChange={(checked) =>
+                    handleInputChange("includeVideo", checked)
+                  }
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label>Deficulty Level</label>
+                <Select
+                  onValueChange={(value) => handleInputChange("level", value)}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Deficulty Level" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="beginner">Beginner</SelectItem>
+                    <SelectItem value="intermediate">Intermediate</SelectItem>
+                    <SelectItem value="advanced">Advance</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label>Category</label>
+                <Input
+                  placeholder="Enter Category(sperated by Comma)"
+                  onChange={(event) =>
+                    handleInputChange("category", event?.target.value)
+                  }
+                />
+              </div>
+
+              <div className="mt-6">
+                <Button
+                  className="flex items-center w-full"
+                  onClick={onGenerate}
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <Loader2Icon className="animate-spin" />
+                  ) : (
+                    <Sparkle className="mr-2" />
+                  )}{" "}
+                  Generate Course
+                </Button>
+              </div>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
-
-export default AddNewCourseDialog
+export default AddNewCourseDialog;
